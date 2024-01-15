e8a552a9b6189285110286163b39a26a6106f78f
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-01-15 18:56:14 -0500

    T1
    1. Primer Proyecto en Visual Studio

---
+++ M1_ObjectOrientedProgramming/T1/OOP.ConsoleExapmle/OOP.ConsoleExapmle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace OOP.ConsoleExapmle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // Escribir en consola
+
+            Animal animal = new Animal();
+            animal.AnimalSound();
+
+            Console.ReadKey(); // Leer cualquier tecla para continuar
+            
+        }
+    }
+}
