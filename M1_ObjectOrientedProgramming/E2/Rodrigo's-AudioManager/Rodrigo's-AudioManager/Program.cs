b16730eb9ba8533b0a92a5fcfd35fe8c9a81fa7e
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-01-16 16:40:04 -0500

    T2
    E2. Herencia y encapsulacion

---
+++ M1_ObjectOrientedProgramming/E2/Rodrigo's-AudioManager/Rodrigo's-AudioManager/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using vehiculo;
+
+
+namespace Rodrigo_s_AudioManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Type the vehicle!"); // Escribir en consola
+                Console.WriteLine("Type quit to exit the program");
+                Console.WriteLine("Welcome to my Audio Manager");
+
+                string animalType = Console.ReadLine().ToLower();
+
+                Vehiculo vehiculo = null;
+                switch (animalType)
+                {
+                    case "train":
+                        vehiculo = new tren();
+                        break;
+                    case "car":
+                        vehiculo = new Carro();
+                        break;
+                    case "truck":
+                        vehiculo = new Camion();
+                        break;
+                    case "quit":
+                        Console.WriteLine("Exiting the program.");
+                        return;
+                    default:
+                        Console.WriteLine("Vehicle not found!");
+                        System.Threading.Thread.Sleep(2000);
+                        break;
+                }
+                if (vehiculo != null)
+                {
+
+                    vehiculo.autoSound();
+                }
+            }
+        }
+    }
+}
