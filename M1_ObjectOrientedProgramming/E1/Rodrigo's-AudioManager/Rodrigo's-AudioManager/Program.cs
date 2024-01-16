fd019b537b87c982258ca3bc704262bd9a48ef3f
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-01-15 19:23:55 -0500

    E1
    Estructura de proyectos en .NET

---
+++ M1_ObjectOrientedProgramming/E1/Rodrigo's-AudioManager/Rodrigo's-AudioManager/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nuget;
+
+
+namespace Rodrigo_s_AudioManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Mundo!"); 
+            Console.WriteLine("Este es mi Manager de Audio");
+
+            NAudio Nuget = new NAudio();
+
+            Nuget.NugetSound();
+
+            Console.ReadKey(); 
+        }
+    }
+}
