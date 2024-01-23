bc4267fadfee47accf1cffea373c42b51ff7c5a5
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-01-22 20:08:22 -0500

    T4
    Realización del T4

---
+++ M2_WebDevelopment/T4/DataTypes/DataTypes.Web/wwwroot/JavaScript.js
@@ -0,0 +1,24 @@
+﻿var element = document.getElementById("example");
+var textExample = "this is a text";
+var aNumber = 10.333;
+
+//textExample = false;
+
+var complexObject = {
+    name: "Israel",
+    lastname: "Pérez",
+    age: 35
+};
+var text1 = "Text";
+var text2 = 'Text'; 
+var ul = "<ul>";
+
+//for (var i = 0; i < textExample.length; i++) {
+    //ul += ("<li>" + textExample[i]+"</li>")
+//}
+
+//ul += "</ul>"
+
+var data = "13333"
+
+element.innerHTML = typeof (parseInt(data))
---
+++ M2_WebDevelopment/T4/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+String text = "This is a String";
+int age  = 35;
+DateTime now = DateTime.Now;
+double ammount  = 0;
+
+String string2 = text + " a second String " + age + now + ammount;
+String string3 = string.Format("The age is {0}, the time is {1}, and the amount is {2}",age,now,ammount);
+String string4 = $"The age is {age}, the time is {now}, and the amount is {ammount}";
+
+char sampleChar ='A';
+char[] arrayChar = string4.ToCharArray();
+
+ammount = (double)10/(double)3;
+
+DateTime dateTime = new DateTime(2000,1,1);
+
+var timeStamp = now - dateTime;
+//var otherText = "This is a text";
+
+Console.WriteLine(timeStamp.TotalMilliseconds);
+
+String test = "15";
+String booleanValue = "true";   
+bool isTrue = bool.Parse(booleanValue);
+
+age = int.Parse(test);
+Console.ReadKey();
