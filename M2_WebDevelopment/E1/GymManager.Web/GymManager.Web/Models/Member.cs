d7f792c15c4888c8bd3c3066402b5530fce9b603
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-01-23 21:01:48 -0500

    Correciones de E1

---
+++ M2_WebDevelopment/E1/GymManager.Web/GymManager.Web/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿namespace GymManager.Web.Models
+{
+    public class Member
+    {
+        public string name { get; set; }
+
+        public string lastName { get; set; }
+
+        public DateTime birth { get; set; }
+
+        public int cityId { get; set; }
+
+        public string email { get; set; }
+
+        public bool allowNewsLetter { get; set; }
+    }
+}
