e1d59b76ec8cc04f52a6a2526d71628234436683
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-02-08 09:50:23 -0500

    E2

---
+++ M3_NetProjectsWebApi/E2/GymManager.Web/GymManager.Core/Members/Member.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.Core.Members
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        [StringLength(15)] 
+        [Required(ErrorMessage ="Debe ingresar el nombre del miembro")]
+        public string name { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string lastName { get; set; }
+
+        [BindProperty, DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}",ApplyFormatInEditMode = true)]
+        public DateTime birth { get; set; }
+
+        [Range(1,100)]
+        public int cityId { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string email { get; set; }
+
+        public bool allowNewsLetter { get; set; }
+    }
+}
---
+++ M3_NetProjectsWebApi/E2/GymManager.Web/GymManager.Web/Program.cs
@@ -0,0 +1,30 @@
+using GymManagerApplicationServices.Members;
+using GymManagerApplicationServices.MembershipTypes;
+using Microsoft.AspNetCore.Builder;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+CultureInfo cultureInfo = new CultureInfo("es-ES"); 
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddTransient<IMembersAppService, MembersAppService>();
+builder.Services.AddTransient<IMembershipTypesAppService, MembershipTypesAppService>();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "membersin",
+    pattern: "{controller=Attendance}/{action=Index}/{id?}");
+
+app.Run();
