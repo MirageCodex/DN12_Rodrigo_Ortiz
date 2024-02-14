3e326617031516f937ba30bce440bac85e03adb0
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-02-14 14:07:56 -0500

    E4
    Serilog

---
+++ M3_NetProjectsWebApi/E4/GymManager.Web/GymManager.Web/Program.cs
@@ -0,0 +1,66 @@
+using GymManager.DataAccess;
+using GymManager.Web.Controllers;
+using GymManagerApplicationServices.Members;
+using GymManagerApplicationServices.MembershipTypes;
+using GymManagerApplicationServices.Navigation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.AspNetCore;
+using Serilog.Configuration;
+using Serilog.Filters;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.File("Logs/log.txt")
+    .WriteTo.Console()
+    .Filter.ByIncludingOnly(Matching.FromSource<MembersController>())
+    .CreateLogger();
+
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+    .ReadFrom.Configuration(hostingContext.Configuration)
+    .WriteTo.Console());
+
+builder.Services.AddLogging();
+builder.Host.UseSerilog();
+
+
+CultureInfo cultureInfo = new CultureInfo("es-ES");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+string connectionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<GymManagerContext>(options =>
+options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<GymManagerContext>();
+
+builder.Services.ConfigureApplicationCookie(options => options.LoginPath = "/Account/LogIn");
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddTransient<IMembersAppService, MembersAppService>();
+builder.Services.AddTransient<IMembershipTypesAppService, MembershipTypesAppService>();
+builder.Services.AddTransient<IMenuAppService, MenuAppService>();
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+app.UseRouting();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ M3_NetProjectsWebApi/E4/GymManager.Web/GymManagerApplicationServices/MembershipTypes/MembershipTypesAppService.cs
@@ -0,0 +1,48 @@
+﻿using GymManager.Core.Members;
+using GymManager.Core.MembershipType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagerApplicationServices.MembershipTypes
+{
+    public class MembershipTypesAppService : IMembershipTypesAppService
+    {
+        private static List<MembershipType> Memberships = new List<MembershipType>();
+        public int AddMembershipTypes(MembershipType membershiptype)
+        {
+            Random random = new Random();
+            membershiptype.idMembership = random.Next();
+
+            Memberships.Add(membershiptype);
+            return membershiptype.idMembership;
+        }
+
+        public void DeleteMembershipType(int membershipId)
+        {
+            var m = Memberships.Where(x => x.idMembership == membershipId).FirstOrDefault();
+            Memberships.Remove(m);
+        }
+
+        public void EditMembershipType(MembershipType membershiptype)
+        {
+            var m = Memberships.Where(x => x.idMembership == membershiptype.idMembership).FirstOrDefault();
+            m.name = membershiptype.name;
+            m.cost = membershiptype.cost;
+            m.duration = membershiptype.duration;
+        }
+
+        public MembershipType GetMembershipType(int membershipId)
+        {
+            var m = Memberships.FirstOrDefault(x => x.idMembership == membershipId);
+            return m;
+        }
+
+        public List<MembershipType> GetMembershipTypes()
+        {
+            return Memberships;
+        }
+    }
+}
