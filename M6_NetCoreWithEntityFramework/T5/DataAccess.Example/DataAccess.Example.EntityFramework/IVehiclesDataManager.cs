64c705f5cd065989229473108eca923000c2cdd9
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-03-12 16:52:55 -0500

    T5

---
+++ M6_NetCoreWithEntityFramework/T5/DataAccess.Example/DataAccess.Example.EntityFramework/IVehiclesDataManager.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Example.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Example.EntityFramework
+{
+    public interface IVehiclesDataManager
+    {
+        void Insert(Vehicles vehicle);
+
+        void Update(Vehicles vehicle);
+
+        Vehicles Get(int id);
+
+        IList<Vehicles> GetAll();
+
+        void Delete(int id);
+    }
+}
