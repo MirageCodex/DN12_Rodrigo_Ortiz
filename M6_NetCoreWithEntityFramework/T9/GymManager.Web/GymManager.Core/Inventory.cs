dc5551f01db593c776bdb34803a4585a155548d1
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-03-18 16:34:49 -0500

    T9

---
+++ M6_NetCoreWithEntityFramework/T9/GymManager.Web/GymManager.Core/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.GymManager.Core
+{
+    public class Inventory
+    {
+        [Key]
+        [Column("idinventory")]
+        public int Id { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+        /*[ForeignKey("MesureTypes")]
+        [Column("mesuretypes_idmesuretypes")]
+        public int MesureTypeId { get; set; }*/
+        public MesureTypes Mesure { get; set; }
+
+        /*[ForeignKey("ProductTypes")]
+        [Column("producttypes_idproducttypes")]
+        public int ProductTypeId { get; set; }*/
+        public ProductTypes Product { get; set; }
+
+        public List<Sales> Sales { get; set; }
+
+        public Inventory()
+        {
+            Sales = new List<Sales>();
+        }
+    }
+}
---
+++ M6_NetCoreWithEntityFramework/T9/GymManager.Web/GymManagerApplicationServices/EquipmentTypes/IEquipmentTypeAppService.cs
@@ -0,0 +1,18 @@
+﻿using GymManager.Core.EquipmentTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagerApplicationServices.EquipmentTypes
+{
+    public interface IEquipmentTypeAppService
+    {
+        Task<int> AddEquipmentTypeAsync(EquipmentType equipmentType);
+        Task DeleteEquipmentTypeAsync(int equipmentId);
+        Task EditEquipmentTypeAsync(EquipmentType equipmentType);
+        Task<EquipmentType> GetEquipmentTypeAsync(int equipmentId);
+        Task<List<EquipmentType>> GetEquipmentTypesAsync();
+    }
+}
