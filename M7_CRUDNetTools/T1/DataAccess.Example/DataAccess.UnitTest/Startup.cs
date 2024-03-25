d94c57c08acc5ffbd13d9ddc14bfa61f91cc3d2b
MirageCodex <130515570+MirageCodex@users.noreply.github.com>
2024-03-25 12:39:52 -0500

    T0 y T1

---
+++ M7_CRUDNetTools/T1/DataAccess.Example/DataAccess.UnitTest/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Example.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataAccess.UnitTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddDbContext<VehiclesDataContext>(options => options.UseInMemoryDatabase("DataTest"));
+
+            services.AddTransient<IQueryExample, QueriesExample>();
+
+            services.AddTransient<IColorsDataManager, ColorsDataManager>();
+
+            services.AddTransient<IVehiclesDataManager, VehiclesDataManager>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
